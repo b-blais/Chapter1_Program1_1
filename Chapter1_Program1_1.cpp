4003a53192a0e38d1832c40d072d1eae9ea0f015
brion_e0oi9kc <brion_e0oi9kc@MSI>
2024-08-26 11:25:44 -0400

    Add project files.

---
+++ Chapter1_Program1_1.cpp
@@ -0,0 +1,33 @@
+/*
+Title: Chapter 1 Assignment 3 - First C++ Program
+File Name : Chapter2Assign3_Brion_Blais
+Programmer : Brion Blais
+Date : 08 / 2024
+Requirements : Install Microsoft Visual Studio following the walkthrough guide and screenshot the intial program and compile result to attach to Canvas. Attach both screenshots and submit the assignment.
+*/
+
+// Chapter1_Program1_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+// Preferred practice to include the std namespace
+using namespace std;
+
+int main()
+{
+    // Removing the 'std', as it is included with the namespace
+    cout << "Hello World!\n";
+    // Programs should always have a return statement
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
